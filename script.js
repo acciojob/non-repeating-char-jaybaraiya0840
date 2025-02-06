d6b2a49b97397ce971a0f408765ec114b93753b6
jaybaraiya0840 <jaybaraiya0802@gmail.com>
2025-02-06 13:02:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let count={};
+	for (let char of str) {
+        Count[char] = (Count[char] || 0) + 1;
+    }
+	for (let char of str) {
+        if (Count[char] == 1) {
+            return char; 
+        }
+    }
+    return null;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+}
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
